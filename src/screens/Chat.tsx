13dcfd883892b68cf6cf9b762cbb0cd919bfabbf
Piyush <piyush41y08h@gmail.com>
2022-03-25 15:35:05 +0530

    fix layout

--- src/screens/Chat.tsx
+++ src/screens/Chat.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {View} from 'react-native';
-import {FlatList} from 'react-native-gesture-handler';
+import {FlatList, ScrollView} from 'react-native-gesture-handler';
 import {Appbar, IconButton, Text, TextInput} from 'react-native-paper';
 import {useQuery} from 'react-query';
 import {useAuth} from '../contexts/AuthContext';
@@ -34,7 +34,10 @@ export default function Chat({route, navigation}) {
   }
 
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+      }}>
       <Appbar.Header style={{backgroundColor: '#25D366'}}>
         <Appbar.BackAction
           color="white"
@@ -44,50 +47,65 @@ export default function Chat({route, navigation}) {
         />
         <Appbar.Content title={name} titleStyle={{color: 'white'}} />
       </Appbar.Header>
-      <View>
-        <TextInput
-          placeholder="Type a message..."
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <FlatList
           style={{
+            flex: 1,
+          }}
+          data={messages.data}
+          renderItem={({item}) => (
+            <Text
+              style={{
+                backgroundColor:
+                  item.sender === currentUser?.username ? '#25D366' : '#fff',
+                color: item.sender === currentUser?.username ? '#fff' : '#000',
+                padding: 10,
+                margin: 10,
+                borderRadius: 10,
+              }}>
+              {item.text}
+            </Text>
+          )}
+          keyExtractor={item => item.id}
+        />
+        <View
+          style={{
+            height: 'auto',
             width: '100%',
-            backgroundColor: '#fff',
-            borderRadius: 10,
-            padding: 10,
-            paddingVertical: 0,
-            margin: 10,
-          }}
-          value={text}
-          onChangeText={setText}
-        />
-        <IconButton
-          icon="send"
-          color="#25D366"
-          size={30}
-          onPress={onSendPressed}
-          style={{
-            position: 'absolute',
-            right: 10,
-            bottom: 10,
-          }}
-        />
-      </View>
-
-      <FlatList
-        data={messages.data}
-        renderItem={({item}) => (
-          <Text
+            position: 'relative',
+            borderWidth: 1,
+            borderTopColor: '#25D366',
+            borderRightWidth: 0,
+            borderLeftWidth: 0,
+            borderBottomWidth: 0,
+          }}>
+          <TextInput
+            placeholder="Type a message..."
             style={{
-              backgroundColor:
-                item.sender === currentUser?.username ? '#25D366' : '#fff',
-              color: item.sender === currentUser?.username ? '#fff' : '#000',
-              padding: 10,
-              margin: 10,
+              width: '100%',
+              backgroundColor: '#fff',
               borderRadius: 10,
-            }}>
-            {item.text}
-          </Text>
-        )}
-        keyExtractor={item => item.id}
-      />
+              paddingVertical: 0,
+            }}
+            value={text}
+            onChangeText={setText}
+          />
+          <IconButton
+            icon="send"
+            color="#25D366"
+            size={30}
+            onPress={onSendPressed}
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: 0,
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
